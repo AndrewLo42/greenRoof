32ca9a7b389d62cce6c0910f48f37fd03a010fb4
AndrewLo42 <andrewclo42@gmail.com>
2020-09-14 01:23:57 -0700

    tweaked stylings of mobile menu

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -13,7 +13,14 @@ function Header() {
         <span></span>
         <span></span>
         <span></span>
+        <ul id="menu">
+          <li className="column text-center">
+            <a className="mobile-menu-link" href="/about" > About Us</a>
+            <a className="mobile-menu-link" href="/sign-petition" > Sign the Petition!</a>
+          </li>
+        </ul>
       </div>
+
       <div className="links-container full-screen-header">
         <a className="header-link" href="/about">About</a>
         <a className="header-link" href="/sign-petition">Petition</a>
